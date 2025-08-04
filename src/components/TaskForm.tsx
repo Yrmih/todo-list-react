3fd1601c41584402860992cc8074e0fc945baf6d
Yrmih Ian <yrmihiangoncalvesdelima@gmail.com>
2025-08-03 23:51:56 -0300

    refactor: aplicando destruturação no componente de form

--- src/components/TaskForm.tsx
+++ src/components/TaskForm.tsx
@@ -1,11 +1,18 @@
+interface ItaskProps {
+  btnText: string
+}
 
-
-const TaskForm = () => {
+const TaskForm = ({btnText}: ItaskProps) => {
     return <form>
       <div>
         <label htmlFor="title">Título:</label>
         <input type="text" name="title" placeholder="Título da tarefa" />
       </div>
+      <div>
+        <label htmlFor="difficulty">Dificuldade:</label>
+        <input type="text" name="title" placeholder="Dificuldade da tarefa" />
+      </div>
+      <input type="submit" value={btnText} />
     
     </form>
   }
