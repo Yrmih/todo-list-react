3556e1e1b3d1b69435d4de6b706c4f0f3d1e097d
Yrmih Ian <yrmihiangoncalvesdelima@gmail.com>
2025-08-03 22:48:05 -0300

    feat: criando modulos css para o app.

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 // components
 import Footer from "./components/Footer";
 import Header from "./components/Header";
+// css styles modules
+import styles from "./styles/App.module.css";
 
 function App() {
   return (
     <div>
       <Header />
-      <h1>Conteúdo...</h1>
+      <main className={styles.main}>
+        <h1>Conteúdo...</h1>
+      </main>
       <Footer />
     </div>
   );
