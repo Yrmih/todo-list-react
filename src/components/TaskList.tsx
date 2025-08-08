31c1331f081eb98edb3ddbf4cd941ed72390f3f2
Yrmih Ian <yrmihiangoncalvesdelima@gmail.com>
2025-08-08 01:24:21 -0300

    feat - adicionando icones do bootstrap no html de tasklist

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -15,8 +15,12 @@ const TaskList = ({ taskList }: IAppProps) => {
           <div className={styles.task} key={task.id}> 
             <div>
               <h4>{task.title}</h4>
+              <p>Dificuldade: {task.difficulty}</p>
             </div>
-            <div></div>
+            <div>
+              <i className="bi bi-pencil"></i>
+              <i className="bi bi-trash"></i>
+            </div>
           </div> // o iterar sobre a lista de tarefas e exibir cada tarefa. e exibir cada tarefa.
         ))
       ) : (
