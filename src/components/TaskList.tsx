22a1d225eb32fbefb01d142e0e82f86b6395fe61
Yrmih Ian <yrmihiangoncalvesdelima@gmail.com>
2025-08-08 01:57:02 -0300

    feat - terminando de ajustar os estilos do tasklist

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -17,7 +17,7 @@ const TaskList = ({ taskList }: IAppProps) => {
               <h4>{task.title}</h4>
               <p>Dificuldade: {task.difficulty}</p>
             </div>
-            <div className={styles.actions}> //
+            <div className={styles.actions}>
               <i className="bi bi-pencil"></i>
               <i className="bi bi-trash"></i>
             </div>
