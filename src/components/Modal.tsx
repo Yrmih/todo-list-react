e5dbd31699765d757d88b29a465922fe5d082024
Yrmih Ian <yrmihiangoncalvesdelima@gmail.com>
2025-08-12 22:13:46 -0300

    fix - reconstrução do escopo do code de modal

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -3,22 +3,7 @@
 export interface Props {
 }
 
-
-
-export default class App extends React.Component<IAppProps, IAppState> {
-  constructor(props: Props) {
-    super(props);
-
-    this.state = {
-    }
-  }
-
-  public render() {
-    return (
-      <div>
-        
-      </div>
-    );
-  }
+const Modal = (props: Props) => {
+  
 }
 
