720ef4b3e3201086a42b643aef682fefcc87e74e
Yrmih Ian <yrmihiangoncalvesdelima@gmail.com>
2025-08-12 22:12:56 -0300

    feat - criação de modal escopo

---
+++ src/components/Modal.tsx
@@ -0,0 +1,24 @@
+
+
+export interface Props {
+}
+
+
+
+export default class App extends React.Component<IAppProps, IAppState> {
+  constructor(props: Props) {
+    super(props);
+
+    this.state = {
+    }
+  }
+
+  public render() {
+    return (
+      <div>
+        
+      </div>
+    );
+  }
+}
+
