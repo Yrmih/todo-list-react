ad5379b44c70bc0715ace6af932776d788f22bbd
Yrmih Ian <yrmihiangoncalvesdelima@gmail.com>
2025-08-12 23:43:10 -0300

    feat - criar closemodal para fecher o modal apenac com o click no mouse na fade

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -13,7 +13,7 @@ const Modal = ({children}: Props) => {
 
   return (
     <div id="modal">
-      <div className={style.fade}>
+      <div className={style.fade} onClick={closeModal}>
       </div>
       <div className={style.modal}>
         <h2>Texto Modal</h2>
